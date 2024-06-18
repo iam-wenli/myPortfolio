8dd4d89aa0b54b6398ffd2155095dbcaa1e844b4
Wen Li Leong <wlleong87@gmail.com>
2024-06-18 22:59:53 +0800

    Cannot GET / error rectified. Updated serverjs,vite config, and run build again

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ const helmet = require('helmet');
 app.listen(Port,()=>{
     console.log(`Listening on port ${Port}`);
 });
-app.use(express.static(path.join(__dirname,'client/dist')
+app.use(express.static(path.join(__dirname,'client','dist')
 ));
+app.get('*',(req,res)=> {
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+});
 app.use(helmet());
