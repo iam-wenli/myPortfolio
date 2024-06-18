82948c99b92a6a11374de7501f56fb40d1515a18
Wen Li Leong <wlleong87@gmail.com>
2024-06-19 00:18:28 +0800

    Handle root route added

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ app.listen(Port,()=>{
 });
 app.use(express.static(path.join(__dirname,'client','dist')
 ));
+// Handle root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+// Handle all other routes (for a single-page application)
 app.get('*',(req,res)=> {
     res.sendFile(path.join(__dirname,'client','dist','index.html'));
 });
