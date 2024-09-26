f5b280665a3b0f7cde17012a7e3aeb8b36417db5
Wen Li Leong <wlleong87@gmail.com>
2024-09-26 13:04:33 +0800

    Added changes to Welcome, Introduction, and Skills components

--- server/client/src/components/Introduction.jsx
+++ server/client/src/components/Introduction.jsx
@@ -12,9 +12,9 @@ function Introduction() {
   });
 
   return (
-    <div className="font-serif p-12 bg-orange-200 h-screen">
+    <div className="font-serif px-16 py-10 bg-orange-200 h-screen">
       <h1 className='text-left font-bold text-2xl py-3 pb-10 sm:text-4xl'>Introduction</h1>
-      <p className='font-normal text-justify text-base sm:text-xl leading-7 first-letter:text-7xl first-letter:font-bold first-letter:text-rose-900  first-letter:float-left first-letter:mr-3'>
+      <p className='font-normal text-justify text-base sm:text-xl leading-6 sm:leading-7 first-letter:text-5xl sm:first-letter:text-7xl first-letter:font-bold first-letter:text-rose-900  first-letter:float-left first-letter:mr-2 sm:first-letter:mr-3'>
         Wen Li is my name. My passion in web development was first uncovered in late 2019, when I was encouraged to acquire programming knowledge for the purpose of infusing AI in my job.  
          <br/><br /> Since then, i began to learn the ropes for developing websites.  In my free time, I had watched countless hours of videos, participated in developers community discussion, 
         and done online research in due diligent. In 2022, i had participated in digital design internship, and in 2024 obtained a Diploma in Computer Science and Engineering.
